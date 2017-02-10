5c1c54d0fe294e51b2d1723f4a5e246193d1c06f
Nicolas Calle <ncalle@ucsd.edu>
2017-02-10 10:47:44 -0800

    clicking between strings

--- guitar-app/src/Tab.js
+++ guitar-app/src/Tab.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
 import './Tab.css'
-import Line from 'react-line';
+//var Line = require('react-line');
 class String extends Component {
+    constructor(props){
+        super(props);
+        this.state = {test: -1};
+    }
     render() {
-        return <p>{this.props.num} string</p>
+        var color = this.props.active ? "red" : "black";
+        var stringStyle = {"borderColor": color}
+        return (
+        <div onClick={() => this.handleClick()} className="String" style={stringStyle}>
+        </div>
+        )
+    }
+    handleClick() {
+        //console.log("hello")
+        console.log(this.props.num);
+        this.props.handleClick();
     }
 }
 
 class Tab extends Component {
+    constructor(props){
+        super(props);
+        this.state = {activeString: -1};
+    }
+
     render() {
         return (
             <div className="Tab-frame">
-                <String num={1} />
-                <String num={2} />
-                <String num={3} />
-                <String num={4} />
-                <String num={5} />
-                <String num={6} />
+                {this.renderString(1)}
+                {this.renderString(2)}
+                {this.renderString(3)}
+                {this.renderString(4)}
+                {this.renderString(5)}
+                {this.renderString(6)}
             </div>
         )
     }
+    renderString(i) {
+        return <String num={i} handleClick={() => this.stringClick(i)} active={this.state.activeString === i}/>
+    }
+    stringClick(string) {
+        console.log(this.state);
+        console.log(string);
+        this.setState({activeString: string});
+    }
+
 }
 
 export default Tab
