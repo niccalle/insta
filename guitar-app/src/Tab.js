4203653b7562714cc8e64bc435c03ffa93702b4a
Nicolas Calle <ncalle@ucsd.edu>
2017-03-03 10:52:19 -0800

    Horizontal scrolling on tab

--- guitar-app/src/Tab.js
+++ guitar-app/src/Tab.js
@@ -10,15 +10,12 @@ class String extends Component {
         var color = this.props.active ? "red" : "black";
         var stringStyle = {"borderColor": color}
         return (
-        <div onClick={() => this.handleClick()} className="String" style={stringStyle}>
-        </div>
+        <div className="String" style={stringStyle} onClick={this.props.handleClick}></div>
         )
     }
-    handleClick() {
-        //console.log("hello")
-        console.log(this.props.num);
-        this.props.handleClick();
-    }
+    // handleClick() {
+    //     this.props.handleClick();
+    // }
 }
 
 class Tab extends Component {
@@ -28,24 +25,40 @@ class Tab extends Component {
     }
 
     render() {
+        var columns = [];
+        for (var i=0; i < 100; i++) {
+            columns.push(<a href="#" key={i}>{i}</a>);
+        }
         return (
-            <div className="Tab-frame">
-                {this.renderString(1)}
-                {this.renderString(2)}
-                {this.renderString(3)}
-                {this.renderString(4)}
-                {this.renderString(5)}
-                {this.renderString(6)}
+            <div style={{"display":"inline-block"}} className="Whole-tab" id="whole">
+                <div className="scrollmenu">
+                  {columns}
+                </div>
+                <div className="Tab-frame">
+                    {this.renderString(1)}
+                    {this.renderString(2)}
+                    {this.renderString(3)}
+                    {this.renderString(4)}
+                    {this.renderString(5)}
+                    {this.renderString(6)}
+                </div>
+
             </div>
+
         )
     }
     renderString(i) {
-        return <String num={i} handleClick={() => this.stringClick(i)} active={this.state.activeString === i}/>
+        return <String num={i} handleClick={ this.handleClick(i) } active={this.state.activeString === i}/>
     }
-    stringClick(string) {
-        console.log(this.state);
-        console.log(string);
-        this.setState({activeString: string});
+
+    //This binds the click to the TAB component
+    handleClick = (i) => (e) => {
+        // document.getElementById("Tab-container").scrollLeft
+        // THIS GIVES US HOW FAR WE HAVE SCROLLED 
+        console.log("Client X" + e.clientX);
+        console.log("Screen X" + e.screenXc);
+        //document.getElementById("Tab-container").style.
+        this.setState({activeString: i});
     }
 
 }
