a52b8446d3ac24aa487a291039026ef681fe8451
Nicolas Calle <ncalle@ucsd.edu>
2017-03-08 14:04:18 -0800

    adding tabs works now

--- guitar-app/src/Tab.js
+++ guitar-app/src/Tab.js
@@ -29,10 +29,21 @@ class Cell extends Component {
     }
 }
 
+class Note extends Component {
+    render() {
+        var cell = parseInt(this.props.cell, 10)
+        var string = parseInt(this.props.string, 10)
+        var x = "" + ((cell*3.6) + 1.5) + "%";
+        var y = "" + ((string*13.5) + 22.5) + "%";
+        var noteStyle = {"left": x, "top": y}
+        return (<div className="fret" style={noteStyle}>{this.props.fret}</div>)
+    }
+}
 class Tab extends Component {
     state = {
         activeString: -1,
-        activeCell: -1
+        activeCell: -1,
+        notes: []
     }
 
     render() {
@@ -41,6 +52,9 @@ class Tab extends Component {
             columns.push(<Cell key={i} num={i} active={i === this.state.activeCell}></Cell>)
             //columns.push(<a href="#" key={i}>{i%4 + 1}</a>);
         }
+        document.body.addEventListener('keydown', this.handleKeyPress);
+
+
         return (
             <div style={{"display":"inline-block"}} className="Whole-tab" id="whole">
                 <div className="scrollmenu">
@@ -53,7 +67,7 @@ class Tab extends Component {
                     {this.renderString(4)}
                     {this.renderString(5)}
                     {this.renderString(6)}
-                    <div className="fret">1</div>
+                    {this.state.notes}
                 </div>
 
             </div>
@@ -74,7 +88,17 @@ class Tab extends Component {
 
         //The particular cell that we've clicked
         var cellNum = Math.floor(relativeX / 36.34); //TODO: Make this shit reliable
+
+        //JUST WANT TO TEST: Add a note wherever clicked
         this.setState({activeString: i, activeCell: cellNum});
+    }
+
+    handleKeyPress = (event) => {
+        if(event.key < "0" || event.key > "9") return;
+        console.log(event.key);
+        var notes = this.state.notes.slice();
+        notes.push(<Note cell={this.state.activeCell} string={this.state.activeString-1} key={notes.length} fret={event.key}/>)
+        this.setState({notes: notes});
     }
 
 }
