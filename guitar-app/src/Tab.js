dbeb5cf67761e34edcb6b51c048cc47e6b885e3e
Nicolas Calle <ncalle@ucsd.edu>
2017-03-13 11:08:33 -0700

    added more tabbing functionality

--- guitar-app/src/Tab.js
+++ guitar-app/src/Tab.js
@@ -4,7 +4,6 @@ import './Tab.css'
 class String extends Component {
     constructor(props){
         super(props);
-        this.state = {test: -1};
     }
     render() {
         var color = this.props.active ? "red" : "black";
@@ -13,9 +12,6 @@ class String extends Component {
         <div className="String" style={stringStyle} onClick={this.props.handleClick}></div>
         )
     }
-    // handleClick() {
-    //     this.props.handleClick();
-    // }
 }
 
 class Cell extends Component {
@@ -43,7 +39,8 @@ class Tab extends Component {
     state = {
         activeString: -1,
         activeCell: -1,
-        notes: []
+        notesmap: {},
+        counter: 0
     }
 
     render() {
@@ -53,6 +50,15 @@ class Tab extends Component {
             //columns.push(<a href="#" key={i}>{i%4 + 1}</a>);
         }
         document.body.addEventListener('keydown', this.handleKeyPress);
+        var notes = [];
+        //Push all the notes to the array. Easier to render array
+        for (const cell of Object.keys(this.state.notesmap)) {
+            for(var i = 0; i < 6; i++){
+                if(this.state.notesmap[cell][i]){
+                    notes.push(this.state.notesmap[cell][i])
+                };
+            }
+        }
 
 
         return (
@@ -67,7 +73,7 @@ class Tab extends Component {
                     {this.renderString(4)}
                     {this.renderString(5)}
                     {this.renderString(6)}
-                    {this.state.notes}
+                    {notes}
                 </div>
 
             </div>
@@ -89,16 +95,60 @@ class Tab extends Component {
         //The particular cell that we've clicked
         var cellNum = Math.floor(relativeX / 36.34); //TODO: Make this shit reliable
 
-        //JUST WANT TO TEST: Add a note wherever clicked
         this.setState({activeString: i, activeCell: cellNum});
     }
-
+    //What happens when we want to add a tab
     handleKeyPress = (event) => {
-        if(event.key < "0" || event.key > "9") return;
+        var cell = this.state.activeCell;
+        var string = this.state.activeString - 1;
+        var notesmap = this.state.notesmap;
+        var counter = parseInt(this.state.counter);
+        //Directions to move around the tab frame
         console.log(event.key);
-        var notes = this.state.notes.slice();
-        notes.push(<Note cell={this.state.activeCell} string={this.state.activeString-1} key={notes.length} fret={event.key}/>)
-        this.setState({notes: notes});
+        switch(event.key){
+            case "ArrowUp":
+                event.preventDefault();
+                this.setState({activeString: Math.max(1, this.state.activeString-1)})
+                return;
+            case "ArrowDown":
+                event.preventDefault();
+                this.setState({activeString: Math.min(6, this.state.activeString+1)})
+                return;
+            case "ArrowLeft":
+                event.preventDefault();
+                this.setState({activeCell: Math.max(0, this.state.activeCell-1)})
+                return;
+            case "ArrowRight":
+                event.preventDefault();
+                this.setState({activeCell: Math.min(100, this.state.activeCell+1)})
+                return;
+            case "Backspace":
+                event.preventDefault();
+                if(cell.toString() in notesmap){
+                    notesmap[cell.toString()][string] = false;
+                    this.setState({notesmap: notesmap});
+                }
+                return;
+
+        }
+        if(event.key < "0" || event.key > "9") return;
+        //Initialize state variables for easier access lol
+        //If the note is already there
+        if(cell.toString() in notesmap){
+            var prevNote = notesmap[cell.toString()][string];
+            if(prevNote){
+                var fret = parseInt(prevNote.props.fret)*10;
+                notesmap[cell.toString()][string] =
+                    <Note cell={cell} string={string} key={counter} fret={fret+parseInt(event.key)}/>;
+            }
+            else notesmap[cell.toString()][string] = <Note cell={cell} string={string} key={counter} fret={parseInt(event.key)}/>;
+        }
+        else{
+            notesmap[cell.toString()] = new Array(6);
+            notesmap[cell.toString()][string] = <Note cell={cell} string={string} key={counter} fret={parseInt(event.key)}/>;
+        }
+
+        this.setState({activeCell: Math.min(100, cell+1), notesmap: notesmap, counter: ++counter});
     }
 
 }
